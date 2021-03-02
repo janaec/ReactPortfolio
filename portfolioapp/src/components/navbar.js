2c85a045438a6ff3ae3ec9948c09f65d620de21e
email <jjazminclrk@gmail.com>
2021-03-02 06:45:09 -0500

    adding files from yesterday

--- portfolioapp/src/components/navbar.js
+++ portfolioapp/src/components/navbar.js
@@ -4,13 +4,13 @@ import {navbar,NavDropdown,Form, Nav,FormControl,Button} from 'react-bootstrap'
 function Navbar(props) {
     return (
         <Navbar bg="light" expand="lg">
-  <Navbar.Brand href="#home">Janae J. Clark</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#link">Link</Nav.Link>
-      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <Navbar.Brand href="#home">Janae J. Clark</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#link">Link</Nav.Link>
+        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
