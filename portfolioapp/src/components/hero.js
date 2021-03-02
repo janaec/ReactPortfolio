3b3ad1df117f1163b7d4024223463cb88c15826e
email <jjazminclrk@gmail.com>
2021-03-02 11:15:31 -0500

    done with hero

--- portfolioapp/src/components/hero.js
+++ portfolioapp/src/components/hero.js
@@ -1,7 +1,10 @@
 import React from "react";
 import profile from "../images/profile"
 import {Col, Image, Container} from "react-bootstrap";
-import Jumbotron from 'react-bootstrap/Jumbotron'
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import facebook from "../images/facebook.png";
+import linkedin from "../images/linkedin.png";
+import github from "../images/github.png";
 
 
 function Hero() {
@@ -11,6 +14,9 @@ function Hero() {
                 <Col xs={6} md={4}>
                     <Image src={profile} roundedCircle />
                 </Col>
+                <a href="https://www.facebook.com/janaejazmin/" target="_blank" rel="noreferrer"><img src={facebook} className="social-icon" alt="facebook logo"/></a>
+                <a href="https://www.linkedin.com/in/janae-clark/" target="_blank" rel="noreferrer"><img src={linkedin} className="social-icon" alt="linkedin logo"/></a>
+                <a href="https://github.com/janaec" target="_blank" rel="noreferrer"><img src={github} className="social-icon" alt="github logo"/></a>
             </Container>
         </Jumbotron>
         
